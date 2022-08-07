456907960e963cdf0cdb34001ef3eba570513bba
HamletHajiyev <hamletoh@code.edu.az>
2022-08-07 16:09:38 +0400

    page layout structure

---
+++ containers/TrendingSide/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TrendingSection, TrendingContainer } from "./TrendingSideStyled";
+const TrendingSide = () => {
+  return (
+    <TrendingSection>
+      <TrendingContainer></TrendingContainer>
+    </TrendingSection>
+  );
+};
+
+export default TrendingSide;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,16 @@ import Header from "../containers/Header";
 import Footer from "../containers/Footer";
 import Main from "../containers/Main";
 import InterestSide from "../containers/InterestSide";
+import PostSide from "../containers/PostSide";
+import TrendingSide from "../containers/TrendingSide";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Header />
       <Main>
         <InterestSide></InterestSide>
+        <PostSide></PostSide>
+        <TrendingSide></TrendingSide>
       </Main>
       <Footer />
     </div>
