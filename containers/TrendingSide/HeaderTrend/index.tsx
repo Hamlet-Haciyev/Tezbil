a211e8c7ec55a8f89168e87425e1367486580586
Hesenli-Hacaga <hacaga125+1@gmail.com>
2022-08-09 00:52:06 +0400

    Created Trending Tags Side

---
+++ containers/TrendingSide/HeaderTrend/index.tsx
@@ -0,0 +1,12 @@
+import { TrendHeader } from "../TrendingSideStyled";
+
+const HeaderTrend = () => {
+  return (
+    <TrendHeader>
+      <span className="material-symbols-outlined">loyalty</span>
+      <h2>Trending Tags</h2>
+    </TrendHeader>
+  );
+};
+
+export default HeaderTrend;
